57f0b41d263857a9162ae257e659d3f34cf8c0b2
hidanscript <hidanshira2@gmail.com>
2020-03-23 14:28:59 -0300

    Added message input functionalities

--- src/Components/ChatMessages/index.js
+++ src/Components/ChatMessages/index.js
@@ -10,31 +10,52 @@ export default function ChatMessages() {
 
     const userDefault = {
         user_id: 15,
+        username: 'David Marcano'
     }
 
     const [ messages, setMessages ] = useState([]);
+    const [ messageAdded, setMessageAdded ] = useState(0);
     
     useEffect(() => {
-        setMessages(Placeholder.Messages);
-    }, []);
+        if(!messageAdded) {
+            setMessages(Placeholder.Messages);
+        }
+        gotoBottom("messages");
+    }, [messageAdded]);
 
     const renderMessages = ( message , index ) => {
         const classtype = message.user_id === userDefault.user_id ? "message-self" : "message";
         return <Message content={message.content} key={index} username={message.username} ownership={classtype} />;
     }
 
-    const messageList = messages.map(renderMessages)
+    const addMessage = event => {
+        event.preventDefault();
+
+        const content = event.target.message.value;
+        const message = { user_id: userDefault.user_id, username: userDefault.username , content }
+        document.querySelector('#message-value').value = "";
+
+        setMessages(messages.concat(message));
+        setMessageAdded(messageAdded + 1);
+    }
+
+    const gotoBottom = id => {
+        let element = document.querySelector(`#${id}`);
+        element.scrollTop = element.scrollHeight - element.clientHeight;
+     }
+
+    const messageList = messages.map(renderMessages);
 
     return (
         <div className="chat-messages">
 
             <ChannelBar />
 
-            <div className="messages">
+            <div id="messages" className="messages">
                 { messageList }
             </div>
 
-            <MessageInput />
+            <MessageInput callback={addMessage}/>
 
         </div>
     );
--- src/Components/MessageInput/index.js
+++ src/Components/MessageInput/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css';
 
-export default function MessageInput() {
+export default function MessageInput(props) {
     return (
-        <div className="message-input-screen">
+        <form onSubmit={props.callback} className="message-input-screen">
             <i className="far fa-laugh"></i>
-            <input type="text" className="message-input" autoComplete="off" spellCheck="false" placeholder="Type a message here" />
-        </div>
+            <input type="text" className="message-input" id="message-value" autoComplete="off" spellCheck="false" name="message" placeholder="Type a message here" />
+        </form>
     );
 }
